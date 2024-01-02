0e69e589408ead24eaae67cdf05cc10fb857bd7b
Thiago <thiagosiegam@gmail.com>
2024-01-02 12:35:46 -0300

    Refactor code for improved performance and readability

--- compilador.py
+++ compilador.py
@@ -2,9 +2,9 @@ import os
 
 # Path: compilador.py
 
-comando = "g++ -o main main.cpp -std=c++17 -I${workspaceFolder}/SFML/SFML-2.6.1/include -L${workspaceFolder}/SFML/SFML-2.6.1/lib -lsfml-window -lsfml-system -lsfml-graphics"
+comando = "g++ -o main.exe main.cpp -std=c++17 -I${workspaceFolder}/SFML/SFML-2.6.1/include -L${workspaceFolder}/SFML/SFML-2.6.1/lib -lsfml-window -lsfml-system -lsfml-graphics"
 
-executar = "./main"
+executar = "./main.exe"
 
 os.system(comando)
 os.system(executar)
--- main.cpp
+++ main.cpp
@@ -1,83 +1,65 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <string>
 
 int main()
 {
-    // Cria uma nova janela com dimensões 800x600 pixels
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Projetos SFML ");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Projetos SFML");
 
-    // Cria uma instância de sf::Font para carregar a fonte
     sf::Font font;
     if (!font.loadFromFile("SFML/Fonte/arial.ttf")) {
-        // Trate o caso de falha ao carregar a fonte
         return -1;
     }
 
-    // Cria um objeto sf::Text
     sf::Text texto;
-    texto.setFont(font); // Define a fonte para o texto
-    texto.setCharacterSize(24); // Define o tamanho do caractere
-    texto.setFillColor(sf::Color::White); // Define a cor do texto
-    // Use sf::String para suportar UTF-8
+    texto.setFont(font);
+    texto.setCharacterSize(24);
+    texto.setFillColor(sf::Color::White);
     sf::String utf8Text = L"Projetos em C++ com SFML\n";
-    texto.setString(utf8Text);// Define o texto a ser exibido
-    // Define a posição do texto
-    texto.setPosition(300.f, 100.f);
+    texto.setString(utf8Text);
+    texto.setPosition(100, 50);  // Ajuste da posição
 
-    //defino o botao
-    sf::RectangleShape botao(sf::Vector2f(100, 50)); //tamanho do botao 100x50
-    botao.setPosition(300, 300); //posicao do botao
-    botao.setFillColor(sf::Color::Red); //cor do botao
+    std::string opcoes[] = {"Calculadora", "Adivinha o numero", "Jogo da velha", "Sair"};
 
-    //defino o texto do botao
-    sf::Text textoBotao;
-    textoBotao.setFont(font); // Define a fonte para o texto
-    textoBotao.setCharacterSize(24); // Define o tamanho do caractere
-    textoBotao.setFillColor(sf::Color::White); // Define a cor do texto
-    // Use sf::String para suportar UTF-8
-    sf::String utf8TextBotao = L"Botao\n";
-    textoBotao.setString(utf8TextBotao);// Define o texto a ser exibido
-    // Define a posição do texto
-    textoBotao.setPosition(320.f, 315.f);
+    sf::Text opcao[4];  // Mova a declaração do array para fora do loop de eventos
 
+    for (int i = 0; i < 4; i++) {
+        opcao[i].setFont(font);
+        opcao[i].setCharacterSize(24);
+        opcao[i].setFillColor(sf::Color::White);
+        opcao[i].setString(opcoes[i]);
+        opcao[i].setPosition(100, 100 + (i * 50));
+    }
 
-
-    // Loop principal do jogo
     while (window.isOpen())
     {
-        // Processa eventos
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // Fecha a janela quando o usuário clica no botão de fechar
             if (event.type == sf::Event::Closed)
                 window.close();
-            
-            //evento de clique do mouse
+
             if (event.type == sf::Event::MouseButtonPressed) {
-                //verifico se o clique foi dentro do botao
                 sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
                 sf::Vector2f mousePosFloat(static_cast<float>(mousePosition.x), static_cast<float>(mousePosition.y));
 
-                if (botao.getGlobalBounds().contains(mousePosFloat))
-                {
-                    // Ação quando o botão é clicado
-                    std::cout << "Botão clicado!" << std::endl;
+                for (int i = 0; i < 4; i++) {
+                    sf::FloatRect bounds = opcao[i].getGlobalBounds();
+                    if (bounds.contains(mousePosFloat)) {
+                        std::cout << "Clicou na opção: " << opcoes[i] << std::endl;
+                        // Adicione a lógica correspondente à opção clicada
+                    }
                 }
             }
         }
 
-        // Limpa a janela
         window.clear();
-
-        // Desenha o texto na janela
         window.draw(texto);
-        // Desenha o botao na janela
-        window.draw(botao);
-        // Desenha o texto do botao na janela
-        window.draw(textoBotao);
 
-        // Exibe o conteúdo desenhado
+        for (int i = 0; i < 4; i++) {
+            window.draw(opcao[i]);
+        }
+
         window.display();
     }
 
